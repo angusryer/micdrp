538501bc4ffebd1602b937ef64eb97311f0ee3ef
Angus Ryer <angusryer@gmail.com>
2023-01-17 15:15:22 -0500

    moving to new branch

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,66 +1,248 @@
 #!/bin/bash
 
-# command line options:
-# -p production
-# -s staging
-# development ENV is default
-# 
-# get production, staging or development options from command line
-# store appropriate ENV file
-# increment build and/or version numbers for both iOS and Android
-# run build
-# output locations
+# TODOs remaining
+# increment version numbers for iOS
+# run iOS build
 
+# Make sure the local development environment is configured
+export ANDROID_SDK_ROOT=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_SDK_ROOT/emulator
+export PATH=$PATH:$ANDROID_SDK_ROOT/platform-tools
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/zulu-11.jdk/Contents/Home
+
+clientDir=packages/client
+gradlePath=packages/client/android/app/build.gradle
+env="dev"
+androidVersionCode=0
+androidVersionName=""
+nextVersion=0
+iosVersionNumber=0
+metroPort=9000
+keepVersion=0
+
+if [ ! -d "$clientDir" ]; then
+  printf "Please run from the root 'micdrp' folder...\n"
+  exit 0;
+fi
+
+function help () {
+  printf "\n build.sh -e <OPTION> <DEVICE>\n\n"
+  printf "Example for building an Android bundle with Production environment values:"
+  printf "\n\n./build.sh -e prod android\n\n"
+  printf "    OPTION: -e [dev|staging|prod]\n"
+  printf "    DEVICE: [ios|android|all]\n"
+  exit 0;
+}
+
+function die () {
+  printf '%s\n' "$*" >&2
+  exit 1
+}
+
+if [ $# -gt 3 ]; then
+  help
+  exit 1;
+fi
+
+if [ $# -le 2 ]; then
+  help
+  exit 1;
+fi
+
+device=
+for var in "$@"; do
+  [[ $var = 'ios' ]] && device="ios"
+  [[ $var = 'android' ]] && device="android"
+  [[ $var = 'all' ]] && device="all"
+done
+
+if [[ -z $device ]]; then
+  help
+fi
 
 function getAndroidVersionCode () {
-  local androidVersionCode="$(grep -oE "^.\s*versionCode [0-9]+" packages/client/android/app/build.gradle)"
+  local androidVersionCode="$(grep -oE "^.\s*versionCode [0-9]+" $gradlePath)"
   echo $androidVersionCode;
 }
 
 function getAndroidVersionName () {
-  local androidVersionName="$(grep -oE "^.\s*versionName \"[0-9]+.+\"" packages/client/android/app/build.gradle)"
-  if [ "$?" != "0" ]
-    then
-    local androidVersionName="$(grep -oE "^.\s*versionName \"$env [0-9]+.+\"" packages/client/android/app/build.gradle)"
-  fi
+  local androidVersionName="$(grep -oE "^.\s*versionName \".*[0-9]+.+\"" $gradlePath)"
   echo $androidVersionName;
 }
 
-  # a)
-    #   printf "Starting Android emulator...\n"
-    #   androidVersionName=$(getAndroidVersionName)
-    #   androidVersionCode=$(getAndroidVersionCode)
-    #   currentAndroidVersionCode=$(echo $androidVersionCode | tr -dc '0-9')
-    #   nextAndroidVersionCode=$((currentAndroidVersionCode+1))
-    #   printf "[ ] Attempting to change Android build.gradle version from $currentAndroidVersionCode to $nextAndroidVersionCode...\r"
-    #   if [ "$env" == "development" ]
-    #     then
-    #     printf "You must specify a Production (-p) or Preview (-s) environment\n"
-    #     exit 1;
-    #   fi
-    #   if [ "$androidVersionName" == "" ]
-    #     then
-    #       # couldn't find the vanilla number, assume there is an env in it...
-    #       grep -oE "^.\s*versionName \"[0-9]+.+\"" packages/client/android/app/build.gradle | xargs sed -i "" "s/$androidVersionName/versionName \"$env $nextAndroidVersionCode.0\"/g"
-    #     else 
-    #       grep -oE "^.\s*versionName \"$env [0-9]+.+\"" packages/client/android/app/build.gradle | xargs sed -i "" "s/$androidVersionName/versionName \"$env $nextAndroidVersionCode.0\"/g"
-    #   fi
+function incrementAndroidVersion () {
+  androidVersionName=$(getAndroidVersionName)
+  androidVersionCode=$(getAndroidVersionCode)
+  local currentAndroidVersionCode=$(echo $androidVersionCode | tr -dc '0-9')
+  nextVersion=$((currentAndroidVersionCode+1))
 
-    #   sleep 1
-    #   printf "[\xE2\x9C\x94] Attempting to change Android build.gradle version from $currentAndroidVersionCode to $nextAndroidVersionCode...\r"
+  # We only want to increment the build number when we're building for Staging
+  # but the existing build is for Production
 
-    #   # Sanity check
-    #   updatedAndroidVersionName=$(grep -oE "^.\s*versionName \"$env $nextAndroidVersionCode.+\"" packages/client/android/app/build.gradle)
-    #   if [ "$updatedAndroidVersionName" == "" ]
-    #     then
-    #       printf "Unable to change Version Name. Looking for '$env $currentAndroidVersionCode' Please double check android/app/build.gradle\n"
-    #       printf "Quitting without modifying the build.gradle\n"
-    #       exit 3;
-    #   fi
+  local versionNameString=$(echo $androidVersionName | sed 's/versionName //')
 
-    #   # Update the Version Code, if the Name was successful...
-    #   grep -oErl "^.\s*versionCode [0-9]+" packages/client/android/app/build.gradle | xargs sed -i "" "s/$currentAndroidVersionCode/versionCode $nextAndroidVersionCode/g"
-    #   newVersion=$nextAndroidVersionCode;;
-    
-    # i)
-    #   printf "Running iOS simulator...\n";;
+  if [[ $env = "Staging" ]] && [[ $versionNameString = "\"Staging $currentAndroidVersionCode.0\"" ]]; then
+    printf "[\xE2\x9C\x94] Checking for correct build version... SUCCESS\n"
+    nextVersion=$currentAndroidVersionCode
+    return -1;
+  fi
+
+  if [[ $env = "Production" ]] && [[ $versionNameString = "\"Staging $currentAndroidVersionCode.0\"" ]]; then
+    printf "[\xE2\x9C\x94] Checking for correct build version... SUCCESS\n"
+    nextVersion=$currentAndroidVersionCode
+    return -1;
+  fi
+
+  if [[ $env = "Production" ]] && [[ $versionNameString = "\"Production $currentAndroidVersionCode.0\"" ]]; then
+    printf "[\xE2\x9C\x94] Checking for correct build version... SUCCESS\n"
+    nextVersion=$currentAndroidVersionCode
+    return -1;
+  fi
+  
+  printf "[ ] Attempting to change Android build.gradle version from $currentAndroidVersionCode to $nextVersion...\r"
+
+  sed -i "" "s/$androidVersionName/versionName \"$env $nextVersion.0\"/g" $gradlePath
+
+  # Ensure that the version has actually been updated
+  local updatedAndroidVersionName=$(grep -oE "versionName \"$env $nextVersion.+\"" $gradlePath)
+  if [ "$updatedAndroidVersionName" == "" ]; then
+      printf "[x] Attempting to change Android build.gradle version from $currentAndroidVersionCode to $nextVersion... FAILED\n"
+      printf "Error verifying $gradlePath... UNMODIFIED\n"
+      exit 3;
+  fi
+
+  # Update versionCode if versionName change was successful
+  sed -i "" "s/versionCode $currentAndroidVersionCode/versionCode $nextVersion/g" $gradlePath
+  printf "[\xE2\x9C\x94] Attempting to change Android build.gradle version from $currentAndroidVersionCode to $nextVersion... SUCCESS\n"
+  return 0;
+}
+
+function incrementIosVersion () {
+  printf "Incrementing iOS version numbers not implemented...\n"
+}
+
+###
+### We may need a keystore and to sign the bundle
+### https://medium.com/androiddevelopers/building-your-first-app-bundle-bbcd228bf631
+### Run ONCE for every computer that will build and upload bundles to Play Store:
+### debug: keytool -genkey -v -keystore /Users/angusryer/dv/micdrp/packages/client/android/app/debug.keystore -alias androiddebugkey -storepass android -keypass android -keyalg RSA -keysize 2048 -validity 10000 -dname "CN=Android Debug,O=Android,C=US"
+### release: keytool -genkey -v -keystore /Users/angusryer/dv/micdrp/packages/client/android/app/micdrp.keystore -alias androidReleasekey -storepass android -keypass android -keyalg RSA -keysize 2048 -validity 10000 -dname "CN=Android Release,O=Android,C=US"
+### 
+### Run this as part of this script to sign the bundle:
+### jarsigner -keystore $pathToKeystore app-release.aab $keyAlias
+
+function buildAndroid () {
+  printf "[ ] Checking for and removing conflicting bundles...\r"
+  local lowerCaseEnv=$(echo "$env" | tr '[A-Z]' '[a-z]')
+  cd packages/client
+  rm android/app/build/outputs/aab/micdrp-$lowerCaseEnv-$nextVersion.aab 2> /dev/null
+  printf "[\xE2\x9C\x94] Checking for and removing conflicting bundles... SUCCESS\n"
+  # https://github.com/react-native-community/cli/blob/main/docs/commands.md#bundle
+  npx react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res/
+
+  # Remove all drawable extension folder from packages/client/android/app/src/main/res
+  pushd android/app/src/main/res
+  rm -rf drawable-*
+  popd
+
+  pushd android
+  ./gradlew bundleRelease
+  if [ "$?" != "0" ]
+    then
+    echo "ERROR: Build failed!"
+    exit 1;
+  fi
+  popd
+
+  mv android/app/build/outputs/bundle/release/app-release.aab android/app/build/outputs/bundle/release/micdrp-$lowerCaseEnv-$nextVersion.aab
+  printf "\n\n[\xE2\x9C\x94] Your new bundle is ready!\n\n"
+  ls -lah `pwd`/android/app/build/outputs/bundle/release/micdrp-$lowerCaseEnv-$nextVersion.aab
+  return 0;
+}
+
+function buildIos () {
+  printf "Bulding iOS bundles not implemented yet..."
+  return 0;
+}
+
+function buildClient () {
+  if [[ $device = 'all' ]]; then
+    printf "Building iOS and Android client bundles...\n"
+    [ $keepVersion -eq 0 ] && incrementAndroidVersion
+    [ $keepVersion -eq 0 ] && incrementIosVersion
+    buildAndroid
+    buildIos
+  elif [[ $device = 'ios' ]]; then
+    printf "Building iOS client bundle...\n"
+    [ $keepVersion -eq 0 ] && incrementIosVersion
+    buildIos
+  else
+    printf "Building Android client bundle...\n"
+    [ $keepVersion -eq 0 ] && incrementAndroidVersion
+    buildAndroid
+  fi
+  printf "\n-----------------------------------------\n\n"
+  return 0;
+}
+
+printf "\n-----------------------------------------\n\n"
+
+while getopts ':ve:h' option; do
+  case $option in
+    v)
+      keepVersion=1
+    ;;
+    e)
+      if [[ $OPTARG = "staging" ]] || [[ $OPTARG = "prod" ]]; then
+        env=$OPTARG
+      fi
+      case $env in
+        prod)
+          printf "[\xE2\x9C\x94] Using Production environment variables\n"
+          export ENVFILE=.production.env
+          env="Production"
+          buildClient
+        ;;
+
+        staging)
+          printf "[\xE2\x9C\x94] Using Staging environment variables\n"
+          export ENVFILE=.staging.env
+          env="Staging"
+          buildClient
+        ;;
+
+        dev)
+          if [[ $OPTARG != "dev" ]]; then
+            printf "You must specify dev, staging or prod with the -e option\n"
+            exit 1
+          fi
+
+          printf "[\xE2\x9C\x94] Using Development environment variables\n"
+          export ENVFILE=.development.env
+
+          if [[ $device = 'all' ]]; then
+            printf "Building iOS and Android client bundles...\n"
+            buildAndroid
+            buildIos
+          elif [[ $device = 'ios' ]]; then
+            printf "Building iOS client bundle...\n"
+            buildIos
+          else
+            printf "Building Android client bundle...\n"
+            buildAndroid
+          fi
+        ;;
+
+      esac
+    ;;
+
+    h) help;;
+    *) help;;
+    \?) # Invalid option
+      printf "Error: Invalid option\n"
+      help
+      exit 1
+    ;;
+
+  esac
+done
